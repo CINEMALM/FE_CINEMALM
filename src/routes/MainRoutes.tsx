0b15ba9f31764c1afd3ad7ac82f79626823da3bc
duy <duynguyenmanh1111@gmail.com>
2026-07-13 20:04:00 +0700

    feature: add path profile

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,7 +1,9 @@
 import type { RouteObject } from "react-router";
 import MainLayout from "../common/layouts/MainLayout";
 import HomePage from "../pages/client/home/Homepage";
-
+import Profile from "../pages/client/user/Profile";
+import ProfileLayout from "../common/layouts/ProfileLayout";
+import MyTicket from "../pages/client/user/MyTicket";
 export const MainRoutes: RouteObject[] = [
   {
     path: "",
@@ -10,6 +12,20 @@ export const MainRoutes: RouteObject[] = [
       {
         index: true,
         element: <HomePage />,
+      },
+      {
+        path: "profile",
+        element: <ProfileLayout />,
+        children: [
+          {
+            index: true,
+            element: <Profile />,
+          },
+          {
+            path: "ticket",
+            element: <MyTicket />,
+          },
+        ],
       },
     ],
   },
