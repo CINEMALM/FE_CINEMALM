00c51063083cca143d5c7a579d0ba2a34c16a8a1
duy <duynguyenmanh1111@gmail.com>
2026-07-13 17:50:03 +0700

    feature: add base layout & index

--- src/common/layouts/MainLayout.tsx
+++ src/common/layouts/MainLayout.tsx
@@ -1,16 +1,35 @@
 import { Outlet } from "react-router";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import { ConfigProvider, theme, App } from "antd";
 const MainLayout = () => {
   return (
-    <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </>
+    <div className="bg-[#10141b] min-h-screen text-white">
+      <ConfigProvider
+        theme={{
+          algorithm: theme.darkAlgorithm,
+          token: {
+            colorBgContainer: "#10141b",
+            colorPrimary: "#ef4444",
+          },
+          components: {
+            Modal: {
+              contentBg: "transparent",
+              headerBg: "transparent",
+              footerBg: "transparent",
+            },
+          },
+        }}
+      >
+        <App>
+          <Header />
+          <main>
+            <Outlet />
+          </main>
+          <Footer />
+        </App>
+      </ConfigProvider>
+    </div>
   );
 };
 
